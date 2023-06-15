209e296b2a1fc55bbd5e9235dcf670cb1e3e2558
Akashdeep Dey <119613277+Akashdeep41@users.noreply.github.com>
2023-06-15 21:33:16 +0530

    my 1st project

---
+++ js/music-list.js
@@ -0,0 +1,39 @@
+
+let allMusic = [
+  {
+    name: "Tere Vaaste (Zara Hatke Zara Bachke)",
+    artist: "Varun Jain, Sachin Jigar",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Dreamers (FIFA World Cup 2022)",
+    artist: "	Jung Kook",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Intezaar",
+    artist: "	Zaeden",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Stay",
+    artist: " The Kid Laroi, Justin Bieber",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: " Pyaar Hota Kayi Baar Hai ",
+    artist: "Arijit Singh, Pritam, Amitabh Bhattacharya",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  }
+];
